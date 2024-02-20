f59fd02ca76596b0d585667d4c86bcecb4ea0a4c
hallekh <lekhnovska.halyna@gmail.com>
2024-02-20 14:34:00 +0100

    added some comments about params

--- src/js/api.js
+++ src/js/api.js
@@ -4,11 +4,23 @@ class YourEnergyAPI {
   // Private field to store the base URL
   #baseUrl = '';
 
+  /**
+   * @param {string} baseUrl  - The base url for the API.
+   */
+
   // Constructor to initialize the base URL
   constructor(baseUrl) {
     this.#baseUrl = baseUrl;
   }
 
+  /**
+   * All filters. This endpoint returns all the filters available for the exercise search.
+   * @param {Object} query - The parameters for the exercise search.
+   * @param {string} query.filter - The filter to apply to the search.
+   * @param {number} query.page - The page number of the results to return.
+   * @param {number} query.limit - The number of exercises to return.
+   * @returns
+   */
   // Method to fetch filters from the API
   filters(query) {
     // Convert query parameters to URL search params
@@ -24,6 +36,17 @@ class YourEnergyAPI {
     );
   }
 
+  /**
+   * All exercises. This endpoint returns all the exercises available for the provided.
+   * @param {Object} query - The parameters for the exercise search.
+   * @param {string} query.bodypart - The filter that is applied to the search.
+   * @param {string} query.muscles - The filter that is applied to the search.
+   * @param {string} query.equipment - The filter that is applied to the search.
+   * @param {string} query.keyword - The keyword for search by exercise name.
+   * @param {number} query.page - The page number of the results to return.
+   * @param {number} query.limit - The number of exercises to return.
+   * @returns
+   */
   // Method to fetch exercises from the API
   exercises(query) {
     const parameters = new URLSearchParams(query);
@@ -37,10 +60,9 @@ class YourEnergyAPI {
     );
   }
 
-  // Method to fetch quotes from the API
-  quotes(query) {
-    const parameters = new URLSearchParams(query);
-    return fetch(`${this.#baseUrl}/api/quote?${parameters}`).then(response => {
+  // Method to fetch quotes from the API. No paramentrs are required
+  quotes() {
+    return fetch(`${this.#baseUrl}/api/quote`).then(response => {
       if (response.ok) {
         return response.json();
       }
@@ -48,7 +70,8 @@ class YourEnergyAPI {
     });
   }
 
-  // Method to fetch a specific exercise by its ID
+  // Method to fetch a specific exercise by its ID.
+  //* @param {string} id - Exercise ID.
   getExerciseById(id) {
     return fetch(`${this.#baseUrl}/api/exercises/${id}`).then(response => {
       if (response.ok) {
