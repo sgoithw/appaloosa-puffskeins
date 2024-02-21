9e5292632b7b9b54df77cc9b5e1cac83e3050237
hallekh <lekhnovska.halyna@gmail.com>
2024-02-21 21:19:03 +0100

    added added favouritesManager class with methods

---
+++ src/js/favouritesManager.js
@@ -0,0 +1,51 @@
+class FavouritesManager {
+  /**
+   * FavouritesManager's constructor.
+   * initializing new instance with provided parameters.
+   */
+  constructor() {
+    this.favouritesKey = 'favouriteExercises';
+  }
+
+  /**
+   * add exersice to favourites.
+   * @param {object} exercise - exersice object with all parameters.
+   */
+  addExerciseToFavorites(exercise) {
+    const favourites = this.getFavorites();
+    favourites.push(exercise);
+    localStorage.setItem(this.favouritesKey, JSON.stringify(favourites));
+  }
+
+  /**
+   * remove exercise from favourites using its ID
+   * @param {string} exerciseId - exersice ID.
+   */
+  removeExerciseFromFavorites(exerciseId) {
+    let favourites = this.getFavorites();
+    favourites = favourites.filter(exercise => exercise.id !== exerciseId);
+    localStorage.setItem(this.favouritesKey, JSON.stringify(favourites));
+  }
+
+  /**
+   * returns currents list of favourites from localStorage.
+   * @returns {Array} - array of exercises's objects.
+   */
+  getFavorites() {
+    const favouritesJSON = localStorage.getItem(this.favouritesKey);
+    return favouritesJSON ? JSON.parse(favouritesJSON) : [];
+  }
+
+  /**
+   * checks if this particular exersice with this ID in favourites.
+   * @param {string} exerciseId - exersice ID.
+   * @returns {boolean} - true, exersice is in favourites, if not false.
+   */
+  isFavorite(exerciseId) {
+    const favourites = this.getFavorites();
+    return favourites.some(exercise => exercise.id === exerciseId);
+  }
+}
+
+// exporting class FavouritesManager.
+export default FavouritesManager;
