5edb559b9a8e34d1be49e75fb6af7e7700d0ab30
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-02-22 11:49:11 +0100

    loader.js, subscribeForm.js

--- src/js/api.js
+++ src/js/api.js
@@ -110,7 +110,7 @@ class YourEnergyAPI {
    * Method to create a subscription
    * @param data - object that contains email
    * @returns {Promise<Response>}
-   */
+   
   createSubscription(data) {
     return fetch(`${this.#baseUrl}/api/subscription`, {
       method: 'POST',
@@ -125,7 +125,13 @@ class YourEnergyAPI {
       throw new Error('Error creating subscription');
     });
   }
+  */
 }
 
 // Export an instance of YourEnergyAPI initialized with the base URL
 export default new YourEnergyAPI(BASE_URL);
+
+export const createSubscription = async data => {
+  const response = await api.post('/subscription', data);
+  return response.data;
+};
---
+++ src/js/subscribeForm.js
@@ -0,0 +1,54 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { createSubscription } from './api';
+import { loader } from './loader';
+
+const emailInput = document.querySelector('.subscribe-input');
+const submitButton = document.querySelector('.submit-btn');
+submitButton.addEventListener('click', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = emailInput.value.trim();
+  const emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+  if (!emailPattern.test(email)) {
+    iziToast.show({
+      position: 'center',
+      color: 'red',
+      message: `Enter the following sample email - 'test@gmail.com'.`,
+    });
+    emailInput.value = '';
+  }
+
+  const data = {
+    email: email,
+  };
+
+  loader.create();
+
+  createSubscription(data)
+    .then(response => {
+      const respMessage = response.message;
+      iziToast.show({
+        position: 'center',
+        color: 'green',
+        message: `${respMessage}`,
+      });
+      emailInput.value = '';
+    })
+    .catch(err => {
+      if (err.response.status === 409) {
+        iziToast.show({
+          position: 'center',
+          color: 'red',
+          message: `this email ${email} has been already subscribed`,
+        });
+        emailInput.value = '';
+      }
+    })
+    .finally(() => {
+      loader.destroy();
+    });
+}
