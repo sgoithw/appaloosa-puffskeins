653ea7f787d25afaa0100a6b41c88e1137919182
sgoithw <141962074+sgoithw@users.noreply.github.com>
2024-02-24 07:24:46 +0000

    Deploying to gh-pages from @ sgoithw/appaloosa-puffskeins@8811f3d719755ff392dcd72225fa6bedd1439280 🚀

--- assets/modal-51bd5034.js
+++ assets/modal-51bd5034.js
@@ -156,20 +156,20 @@ var O=Object.defineProperty;var B=(t,e,s)=>e in t?O(t,e,{enumerable:!0,configura
 `}}var f,M;const u=class u{constructor(){b(this,f)}getExerciseListHTML(e,s){if(!u.exerciseCardType[s])return console.error(`Unsupported exercise card type '${s}' when trying to create exercise card.`),"";let r;return s===u.exerciseCardType.FAVORITES&&(r=`
         <button class='js-remove move-to-trash-btn' type='button'>
         <svg class='move-to-trash-icon' width='16' height='16'>
-              <use href='./../../img/icons.svg#icon-trash'></use>
+              <use href='./img/icons.svg#icon-trash'></use>
             </svg>
           </button>`),e.map(i=>(s===u.exerciseCardType.HOME&&(r=`
         <div class='rating'>
         <span class='rating-value'>${i.rating}</span>
         <svg class='rating-icon' width='18' height='18'>
-        <use href='./../../img/icons.svg#icon-Star'></use>
+        <use href='./img/icons.svg#icon-Star'></use>
         </svg>
         </div>`),`
       <li class='exercise' data-exercise-id='${i._id}'>
         <button class='main-action-btn card__btn' data-id='${i._id}' type='button'>
           Start
           <svg class='main-action-arrow-icon' width='16' height='16'>
-            <use href='./../../img/icons.svg#icon-arrow-right'></use>
+            <use href='./img/icons.svg#icon-arrow-right'></use>
           </svg>
         </button>
         <div class='top'>
@@ -178,7 +178,7 @@ var O=Object.defineProperty;var B=(t,e,s)=>e in t?O(t,e,{enumerable:!0,configura
         </div>
         <div class='title'>
           <svg class='title-icon' width='14' height='16'>
-            <use href='./../../img/icons.svg#icon-running-stick-figure-svgrepo-com-1'></use>
+            <use href='./img/icons.svg#icon-running-stick-figure-svgrepo-com-1'></use>
           </svg>
           <span class='title-text text-clipped'>${i.name.charAt(0).toUpperCase()+i.name.slice(1)}</span>
         </div>
@@ -207,14 +207,14 @@ var O=Object.defineProperty;var B=(t,e,s)=>e in t?O(t,e,{enumerable:!0,configura
         <li class='exs-pagination-item'>
             <a class='exs-pagination-link' data-page='${s-1} '>
                 <svg class='icon-pagination-arrow' width='20' height='20'>
-                    <use href='../../img/icons.svg#icon-fi-rr-angle-small-left'></use>
+                    <use href='./img/icons.svg#icon-fi-rr-angle-small-left'></use>
                 </svg>
             </a>
         </li>`),n.pages.forEach(o=>{i+=`<li class='exs-pagination-item ${n.page==o?"active":""}'><a class='exs-pagination-link' ${o!=="..."?`data-page="${o}"`:""}>${o}</a></li>`}),s<e&&(i+=`
         <li class='exs-pagination-item'>
             <a class='exs-pagination-link' data-page='${s+1} '>
                 <svg class='icon-pagination-arrow' width='20' height='20'>
-                    <use href='../../img/icons.svg#icon-fi-rr-angle-small-right'></use>
+                    <use href='./img/icons.svg#icon-fi-rr-angle-small-right'></use>
                 </svg>
             </a>
         </li>`),i}};f=new WeakSet,M=function(e,s=1,r=9){e=e>1?e:1;let i=[];if(e>4)i=[s===1?1:s<e-2?s-1:e-4,s<e-2?s===1?2:s:e-3,s<e-2?s===1?3:s+1:e-2,s<e-3?"...":e-1,e];else for(let a=1;a<=e;a++)i.push(a);return{pages:i,page:s<=e?s:1,maxPage:e,nextPage:s<e?s+1:e,prevPage:s>1?s-1:1}},E(u,"exerciseCardType",Object.freeze({HOME:"HOME",FAVORITES:"FAVORITES"}));let y=u;const Y=new y,H=new A;function I(){const t=document.querySelector(".js-menu-container"),e=document.querySelector(".js-open-menu"),s=document.querySelector(".js-close-menu"),r=document.body,i=()=>{const c=e.getAttribute("aria-expanded")==="true"||!1;e.setAttribute("aria-expanded",!c),t.classList.toggle("is-open"),r.classList.toggle("menu-open"),r.classList.contains("menu-open")?n():o()},a=()=>{t.classList.remove("is-open"),e.setAttribute("aria-expanded",!1),r.classList.remove("menu-open"),o()},n=()=>{r.style.overflow="hidden"},o=()=>{r.style.overflow="visible"};e.addEventListener("click",i),s.addEventListener("click",i),r.addEventListener("click",c=>{c.target.closest(".js-menu-container")||c.target.closest(".js-open-menu")||t.classList.contains("is-open")&&a()}),window.matchMedia("(min-width: 768px)").addEventListener("change",c=>{c.matches&&a()})}I();const C="https://your-energy.b.goit.study";var l;class N{constructor(e){b(this,l,"");$(this,l,e)}filters(e){const s=new URLSearchParams(e);return fetch(`${d(this,l)}/api/filters?${s}`).then(r=>{if(r.ok)return r.json();throw new Error("Error fetching filters")})}exercises(e){const s=new URLSearchParams(e);return fetch(`${d(this,l)}/api/exercises?${s}`).then(r=>{if(r.ok)return r.json();throw new Error("Error fetching exercises")})}quotes(){return fetch(`${d(this,l)}/api/quote`).then(e=>{if(e.ok)return e.json();throw new Error("Error fetching qoute")})}getExerciseById(e){return fetch(`${d(this,l)}/api/exercises/${e}`).then(s=>{if(s.ok)return s.json();throw new Error("Error fetching exercise by ID")})}patchRating(e,s){return fetch(`${d(this,l)}/api/exercises/${e}/rating`,{method:"PATCH",headers:{"Content-Type":"application/json"},body:JSON.stringify(s)}).then(r=>{if(r.ok)return r.json();throw new Error("Error patching rating")})}createSubscription(e){return fetch(`${d(this,l)}/api/subscription`,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(e)}).then(s=>{if(s.ok)return s.json();switch(s.status){case 409:throw new Error("Email has been already subscribed");default:throw new Error("Error creating subscription")}})}}l=new WeakMap;const R=new N(C);let v=!1,_,m=null;const p=document.querySelector(".exercises-popup"),h=document.querySelector(".overlay"),D=document.querySelector(".exercises-cards-list");D.addEventListener("click",J);async function J(t){if(t.target.closest(".card__btn"))try{const e=t.target.closest(".card__btn").getAttribute("data-id"),s=await R.getExerciseById(e);m=s,_=e;const r=H.getExerciseDetailsHTML(s);K(r,s),U(),document.querySelector(".exercises-modal-favorite-btn").addEventListener("click",W),document.querySelector(".modal-closed-btn").addEventListener("click",w)}catch(e){console.log(e)}}function U(){const t=window.innerWidth-document.body.offsetWidth+"px";p.classList.remove("hidden"),h.classList.remove("hidden"),document.body.style.paddingRight=t,document.body.style.overflow="hidden"}function K(t,e){p.innerHTML=t,V(e)}function V(t){const e=document.querySelector(".exercises-modal-favorite-btn");v=g.isFavorite(t._id),v?e.innerHTML=F():e.innerHTML=S()}function W(){v=!g.isFavorite(m._id);const t=document.querySelector(".exercises-modal-favorite-btn");document.querySelector(".fav-list"),v?(t.innerHTML=F(),g.addExerciseToFavorites(m)):(t.innerHTML=S(),g.removeExerciseFromFavorites(m._id))}function S(){return`
@@ -236,4 +236,4 @@ var O=Object.defineProperty;var B=(t,e,s)=>e in t?O(t,e,{enumerable:!0,configura
           >
     <use href="/img/icons.svg#icon-trash"></use>
   </svg>`}function w(){p.classList.add("hidden"),h.classList.add("hidden"),document.body.style.paddingRight="0px",document.body.style.overflow="auto"}h.addEventListener("click",function(t){t.target===h&&w()});document.addEventListener("keydown",function(t){t.key==="Escape"&&!p.classList.contains("hidden")&&w()});export{y as E,R as a,Y as e,g as f};
-//# sourceMappingURL=modal-e305b9a2.js.map
+//# sourceMappingURL=modal-51bd5034.js.map
