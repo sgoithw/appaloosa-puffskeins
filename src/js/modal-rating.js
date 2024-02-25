f4c54f702bc3ea3f07f47b9b306b8d2800224b3e
olita16 <119816415+olita16@users.noreply.github.com>
2024-02-25 09:31:06 +0100

    add scroll to top html, css and js

--- src/js/modal-rating.js
+++ src/js/modal-rating.js
@@ -113,38 +113,38 @@ elements.ratingForm.addEventListener('submit', handleSaveForm);
 // //const modalExercises = document.querySelector('.modal-exercises');
 // const modalIsOpen = document.querySelector('.modal-rating');
 // const btnIsClosed = document.querySelector('.rating-close-btn');
-//
+
 // elements.modalExercises.addEventListener('click', onExercisesCardClick);
 // btnIsClosed.addEventListener('click', closeModal);
 // modalIsOpen.addEventListener('click', closeOverlay);
 // document.addEventListener('keydown', onEscClick);
-//
+
 // function onExercisesCardClick(event) {
 //   if (!event.target.closest('.modal-exercises-btn-rating')) {
 //     return;
 //   }
-//
+
 //   modalIsOpen.classList.remove('is-hidden');
 //   elements.modalExercises.classList.add('hidden');
 // }
-//
+
 // // Закриття модального вікна при кліці на кнопку закриття
-//
+
 // function closeModal() {
 //   modalIsOpen.classList.add('is-hidden');
 //   elements.modalExercises.classList.remove('hidden');
 // }
-//
+
 // //Закриття модального вікна при кліці на затемнену область
-//
+
 // function closeOverlay(event) {
 //   if (event.target == modalIsOpen) {
 //     closeModal();
 //   }
 // }
-//
+
 // //Закриття модального вікна при натисканні на клавішу Escape
-//
+
 // function onEscClick(event) {
 //   if (event.key === 'Escape') {
 //     closeModal();
